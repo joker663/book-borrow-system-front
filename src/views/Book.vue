9076b2a4f9ebb11d3208f4606b89d4f4ad53096f
lizhihao <2564047684@qq.com>
2024-05-01 19:56:01 +0800

    feat：新增图书信息管理模块

--- src/views/Book.vue
+++ src/views/Book.vue
@@ -1,14 +1,444 @@
 <template>
-   <div>
-     图书信息管理
-   </div>
+  <el-card shadow="never">
+    <el-form size="small" label-width="auto">
+      <el-row>
+        <el-col :span="5">
+          <el-form-item label="图书名称：">
+            <el-input style="width: 90%" placeholder="请输入图书名称" v-model="listParam.name" suffix-icon="el-icon-search"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="5">
+          <el-form-item label="ISBN：">
+            <el-input style="width: 90%" placeholder="请输入ISBN" v-model="listParam.isbn" suffix-icon="el-icon-search"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="5">
+          <el-form-item label="图书分类：">
+            <el-input style="width: 90%" placeholder="请选择图书分类" v-model="listParam.category" suffix-icon="el-icon-search"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="5">
+          <el-button size="small" @click="resetForm()">重置</el-button>
+          <el-button type="primary" size="small" @click="search()" icon="el-icon-search">查询</el-button>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-button type="success" size="small" @click="add"><i class="el-icon-circle-plus-outline"></i> &nbsp;新增</el-button>
+        <el-popconfirm
+            title="确定删除吗？"
+            @confirm="delBatch"
+        >
+          <el-button type="danger" size="small" icon="el-icon-delete" slot="reference" class="ml-5">批量删除</el-button>
+        </el-popconfirm>
+        <el-button type="primary" @click="exp" class="ml-5" style="margin-left: 7px">导出 <i class="el-icon-top"></i></el-button>
+        <el-upload :action="`http://${serverIp}:8080/book/import/API_006`"
+                   :show-file-list="false"
+                   accept="xlsx"
+                   :on-success="handleExcelImportSuccess"
+                   style="display: inline-block;">
+          <el-button type="primary" class="ml-5" style="margin-right: 7px">导入 <i class="el-icon-bottom"></i></el-button>
+        </el-upload>
+        <el-button
+            class="filter-item"
+            size="small"
+            type="text"
+            icon="el-icon-download"
+            @click="downloadExcelTemplate"
+        >下载导入模板</el-button>
+      </el-row>
+    </el-form>
+
+    <el-table :data="tableData"
+              v-loading="tableLoading"
+              style="margin-top: 12px"
+              :border="true"
+              highlight-current-row
+              :header-cell-style="{background:'#F5F7FA',color:'#606266'}"
+              @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="50"></el-table-column>
+      <el-table-column type="index" label="#" width="50" align="center"></el-table-column>
+      <el-table-column prop="cover" label="图书封面" align="center" min-width="100px">
+        <template slot-scope="scope">
+          <img :src="scope.row.cover" min-width="90" height="90"/>
+        </template>
+      </el-table-column>
+      <el-table-column prop="name" label="图书名称" align="center" min-width="180px"></el-table-column>
+      <el-table-column prop="category" label="图书分类" align="center" min-width="180px"></el-table-column>
+      <el-table-column prop="isbn" label="ISBN" align="center" min-width="170px">
+        <template slot-scope="scope">
+          {{scope.row.isbn || '-'}}
+        </template>
+      </el-table-column>
+      <el-table-column prop="author" label="作者" align="center" min-width="120px"></el-table-column>
+      <el-table-column prop="publisher" label="出版社" align="center" min-width="120px"></el-table-column>
+      <el-table-column prop="totalCount" label="图书总量" align="center" min-width="80px" fixed="right">
+        <template slot-scope="scope">
+          <el-tag type="primary" style="font-size: 13px">{{scope.row.totalCount || '-'}}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column prop="lendCount" label="已借阅" align="center" min-width="80px" fixed="right">
+        <template slot-scope="scope">
+          <el-tag type="warning" style="font-size: 13px">{{scope.row.lendCount}}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column prop="leaveCount" label="可借阅" align="center" min-width="80px" fixed="right">
+        <template slot-scope="scope">
+          <el-tag type="success" style="font-size: 13px">{{scope.row.leaveCount}}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column prop="publishDate" label="出版日期" align="center" min-width="170px">
+        <template slot-scope="scope">
+          {{scope.row.publishDate || '-'}}
+        </template>
+      </el-table-column>
+      <el-table-column prop="description" label="描述" align="center" min-width="900px">
+        <template slot-scope="scope">
+          {{scope.row.description || '-'}}
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" align="center" min-width="180px" fixed="right">
+        <template slot-scope="scope">
+          <el-button size="mini" type="primary" @click="handleEdit(scope.row)" icon="el-icon-edit">编辑</el-button>
+          <el-popconfirm
+              title="确定删除吗？"
+              @confirm="handleDelete(scope.row.id)"
+          >
+            <el-button type="danger" size="mini" icon="el-icon-delete" slot="reference" class="ml-5">删除</el-button>
+          </el-popconfirm>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <el-pagination
+        style="margin-top: 20px"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="listParam.pageNum"
+        :page-sizes="[5, 10, 20, 50]"
+        :page-size="listParam.pageSize"
+        layout="-> ,total, sizes, prev, pager, next, jumper"
+        :total="listParam.total">
+    </el-pagination>
+
+    <el-dialog :close-on-click-modal="false"
+               :title="title"
+               :before-close="handleClose"
+               :visible.sync="dialogFormVisible"
+               width="45%">
+      <el-form label-width="80px" size="small" ref="formRef" :model="form" :rules="rules">
+        <el-form-item label="图书名称" prop="name">
+          <el-input v-model="form.name" autocomplete="off" style="width: 90%;" placeholder="请输入图书名称"></el-input>
+        </el-form-item>
+        <el-form-item label="图书分类" prop="categories">
+          <el-cascader
+              clearable
+              style="width: 90%"
+              :props="{value: 'name', label: 'name'}"
+              v-model="form.categories"
+              :options="categories"
+              placeholder="请选择图书分类">
+          </el-cascader>
+        </el-form-item>
+        <el-form-item label="作者" prop="author">
+          <el-input v-model="form.author" autocomplete="off" style="width: 90%;" placeholder="请输入作者"></el-input>
+        </el-form-item>
+        <el-form-item label="出版社">
+          <el-input v-model="form.publisher" autocomplete="off" style="width: 90%;" placeholder="请输入出版社"></el-input>
+        </el-form-item>
+        <el-form-item label="ISBN">
+          <el-input v-model="form.isbn" autocomplete="off" style="width: 90%;" placeholder="请输入ISBN"></el-input>
+        </el-form-item>
+        <el-form-item label="出版日期">
+          <el-date-picker
+              v-model="form.publishDate"
+              type="date"
+              placeholder="请选择出版日期"
+              value-format="yyyy-MM-dd"
+              format="yyyy-MM-dd">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item label="图书封面">
+          <el-upload
+              class="avatar-uploader"
+              :action="'http://' + serverIp +':8080/file/upload/oss'"
+              :show-file-list="false"
+              :on-success="handleAvatarSuccess"
+          >
+            <img v-if="form.cover" alt="" :src="form.cover" class="avatar">
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+        </el-form-item>
+        <el-form-item label="数量" prop="totalCount">
+          <el-input-number v-model="form.totalCount" controls-position="right" @change="handleTotalCountChange" :min="1"></el-input-number>
+        </el-form-item>
+        <el-form-item label="描述">
+          <el-input type="textarea" v-model="form.description" style="width: 90%;" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="handleClose">取 消</el-button>
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+      </div>
+    </el-dialog>
+  </el-card>
 </template>
 
 <script>
+import {serverIp} from "../../public/config";
+
 export default {
+  data() {
+    return {
+      serverIp: serverIp,
+      title: '',
+      form: {
+        totalCount: 1
+      },
+      dialogFormVisible: false,
+      tableLoading: false, // 显示加载效果
+      formLoading: false,// 表单加载效果
+      listParam: {
+        name: '',
+        isbn: '',
+        category: '',
+        pageNum: 1,
+        pageSize: 5,
+        total: 0
+      },
+      tableData: [],
+      multipleSelection: [],
+      categories: [],
+      rules: {
+        name: [
+          {
+            required: true,
+            trigger: 'blur', // 触发器：失去焦点
+            message: '请输入图书名称'
+          }
+        ],
+        isbn: [
+          {
+            required: true,
+            trigger: 'blur', // 触发器：失去焦点
+            message: '请输入ISBN'
+          }
+        ],
+        categories: [
+          {
+            required: true,
+            trigger: 'blur', // 触发器：失去焦点
+            message: '请选择图书分类'
+          }
+        ],
+        totalCount: [
+          {
+            required: true,
+            trigger: 'blur', // 触发器：失去焦点
+            message: '请输入图书数量'
+          }
+        ]
+      },
+    }
+  },
+  created() {
+    this.initBookCategoryDict()
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    // 初始化表格信息
+    init() {
+      this.request.get("/book/list/API_001", {
+        params: {
+          pageNum: this.listParam.pageNum,
+          pageSize: this.listParam.pageSize,
+          name: this.listParam.name,
+          isbn: this.listParam.isbn,
+          category: this.listParam.category,
+        }
+      }).then(res => {
+        if (res.code === '200'){
+          this.tableData = res.data.list
+          this.listParam.total = res.data.total
+        }
+      })
+    },
+    // 获取图书分类字典
+    initBookCategoryDict(){
+      this.request.get("/category/list/API_001").then(res => {
+        if (res.code === '200'){
+          this.categories = res.data
+        }
+      })
+    },
+    // 搜索
+    search() {
+      this.listParam.pageNum = 1
+      this.listParam.pageSize = 5
+      this.init()
+    },
+    // 重置按钮
+    resetForm() {
+      // 清空表单内容
+      this.listParam.name = ''
+      this.listParam.isbn = ''
+      this.listParam.category = ''
+      // 重置后重新加载数据
+      this.init()
+    },
+    // 新增
+    add() {
+      this.title = '新增图书'
+      this.dialogFormVisible = true
+      this.form.totalCount = 1
+    },
+    // 新增或修改
+    submitForm () {
+      this.$refs.formRef.validate((valid) => {
+        if (valid) { // 如果验证通过的话
+          this.formLoading = true// 表单加载效果
+          this.request.post('/book/addOrUpdate/API_002', this.form).then(res => {
+            this.formLoading = false// 请求到数据之后，关闭表单加载效果
+            if (res.code === '200') {
+              this.$message.success("保存成功")
+              this.handleClose()
+              this.search()
+            } else {
+              this.$message.error(res.msg || '服务器内部异常')
+            }
+          })
+        }
+      })
+    },
+    // 改变当前页的大小
+    handleSizeChange(pageSize) {
+      this.listParam.pageSize = pageSize
+      this.init()
+    },
+    // 页的改变
+    handleCurrentChange(pageNum) {
+      this.listParam.pageNum = pageNum
+      this.init()
+    },
+    // 点击编辑时回显数据
+    handleEdit(row) {
+      this.title = '编辑图书'
+      this.form = JSON.parse(JSON.stringify(row))
+      this.form.categories = this.form.category.split('/')
+      this.dialogFormVisible = true
+    },
+    // 删除
+    handleDelete(id) {
+      this.request.delete("/book/delete/API_003/" + id).then(res => {
+        if (res.code === '200') {
+          this.$message.success("删除成功")
+          this.search()
+        } else {
+          this.$message.error(res.msg || '服务器内部异常')
+        }
+      })
+    },
+    // 批量删除
+    delBatch() {
+      let ids = this.multipleSelection.map(v => v.id)  // [{}, {}, {}] => [1,2,3]
+      if (ids.length < 1){
+        this.$message.warning("请至少选择一个！")
+        return
+      }
+      this.request.post("/book/delete/batch/API_004", ids).then(res => {
+        if (res.code === '200') {
+          this.$message.success("批量删除成功")
+          this.search()
+        } else {
+          this.$message.error("批量删除失败")
+        }
+      })
+    },
+    // 关闭dialog时的操作
+    handleClose () {
+      this.dialogFormVisible = false
+      this.form = {} // 重置form
+      this.$refs.formRef.resetFields()// 在关闭的时候，清除所有的字段和校验信息
+    },
+    // 多选时获取ids
+    handleSelectionChange(val) {
+      this.multipleSelection = val
+    },
+    // 导出
+    exp() {
+      let idsArr = this.multipleSelection.map(v => v.id)  // [{}, {}, {}] => [1,2,3]
+      let param = '?ids='
+      if (idsArr.length > 0){
+        let ids = ''
+        for (let i = 0; i < idsArr.length; i++) {
+          ids += idsArr[i] + ','
+        }
+        if (ids.substring(0,ids.length-1).length > 0){
+          param += ids.substring(0,ids.length-1)
+        }
+      }
+      window.open(`http://${serverIp}:8080/book/export/API_005` + param)
+    },
+    // 导入
+    async handleExcelImportSuccess(res) {
+      if (res.code !== '200'){
+        return
+      }
+      if (res.data.successCount > 0){
+        await this.$message.success(res.data.msgSuccess || '导入成功')
+      }
+      if (res.data.failedCount > 0){
+        await this.$message.error(res.data.msgError || '导入失败')
+      }
+      this.search()
+    },
+    // 下载导入模板
+    downloadExcelTemplate() {
+      window.open(`http://${serverIp}:8080/book/export/template/API_007`)
+    },
+    // 获取排序字段的值
+    handleTotalCountChange(val){
+      this.form.totalCount = val
+    },
+    handleAvatarSuccess(res) {
+      this.form.cover = res
+    },
+    // getPublishDate(date){
+    //   this.form.publishDate = date
+    // }
+  }
 }
+
 </script>
 
 <style>
-
+.avatar-uploader {
+  text-align: left;
+  padding-bottom: 0px;
+  margin-bottom: -5px;
+}
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409EFF;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 130px;
+  height: 130px;
+  line-height: 138px;
+  text-align: center;
+}
+.avatar {
+  width: 80px;
+  height: 80px;
+  display: block;
+}
 </style>
