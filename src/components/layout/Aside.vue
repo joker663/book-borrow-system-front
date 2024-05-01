5a20a027e8d98c3a2231535954f4b3a6c2c01e35
lizhihao <2564047684@qq.com>
2024-05-01 20:12:42 +0800

    fix：修改管理端首页logo

--- src/components/layout/Aside.vue
+++ src/components/layout/Aside.vue
@@ -10,8 +10,8 @@
            router
   >
     <div style="height: 60px; line-height: 60px; text-align: center">
-      <img src="../../assets/logo.png" alt="" style="width: 20px; position: relative; top: 5px; right: 5px">
-      <b style="color: white" v-show="logoTextShow">图书借阅后台管理系统</b>
+      <img src="../../assets/frontlogo.png" alt="" style="width: 45px; position: relative; top: 3px; right: 3px">
+      <b style="color: white" v-show="logoTextShow">图书借阅管理系统</b>
     </div>
     <div v-for="item in menus" :key="item.id">
       <div v-if="item.path">
